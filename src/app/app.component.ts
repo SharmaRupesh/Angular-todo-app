d33b3d3ea7d83f7c80be6ff00f1a053e4ef6230a
SharmaRupesh <sharmarupesh589.com>
2022-08-30 11:28:08 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
+import { Guid } from 'guid-typescript';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'todo-app';
+  todos: Todo[]= [];
+  
+  showCompleted: boolean= false;
+
+  onSubmit(form: NgForm)
+  {
+    if(form.value.title=="")
+    {
+      alert("Please enter something.")
+    }
+    else
+    {
+      let todo = new Todo(Guid.create(),form.value.title, false);
+      this.todos.push(todo);
+      form.resetForm();
+    }
+  }
+  
+  onDelete(id: Guid)
+  {
+    let todo = this.todos.filter(x=>x.id===id)[0];
+    let index = this.todos.indexOf(todo,0);
+    if(index>-1)
+    {
+      this.todos.splice(index,1);
+    }
+    if(this.todos.length==0)
+    {
+      this.showCompleted=false;
+    }
+  }
+
+  onComplete(id: Guid)
+  {
+    this.showCompleted=true;
+    let todo = this.todos.filter(x => x.id===id)[0];
+    todo.isComplete=true; 
+  }
+
 }
